5d7da5aee6a8be01d5d83dab416c6c009967222c
TheWinczi <mwinczewski@tutanota.com>
2022-02-25 17:14:51 +0100

    Views.pt file was updated for new forms views.

--- frontend/src/gameplay/views.py
+++ frontend/src/gameplay/views.py
@@ -1,8 +1,9 @@
 import requests
+from flask import Flask, render_template, Response, flash, redirect, url_for
 
 from frontend.src import app
 from frontend.config import GAMES_SERVER_URL, PLAYERS_SERVER_URL
-from flask import Flask, render_template, Response
+from frontend.src.gameplay.forms import AddPlayerForm, EditPlayerForm, AddGameForm, EditGameForm
 # app = Flask(None)
 
 
@@ -51,9 +52,54 @@ def particular_game_players(game_id: int):
     return render_template("gameplay/game_players.html", game=game, players=players)
 
 
-@app.route("/games/<int:game_id>/edit")
+@app.route("/games/<int:game_id>/players/<int:player_id>/remove")
+def remove_player_from_game(game_id: int, player_id: int):
+    r = requests.put(f"{GAMES_SERVER_URL}api/players/{player_id}", json={
+        "game_id": -1
+    })
+    if r.status_code == 202:
+        flash("Removing from game succeed", 'success')
+    else:
+        flash("Removing from game failed", 'danger')
+    return redirect(url_for("particular_game_players", game_id=game_id))
+
+
+@app.route("/games/<int:game_id>/edit", methods=["GET", "POST"])
 def edit_game(game_id: int):
-    return "<h1>Edit Game</h1>"
+    form = EditGameForm()
+
+    if form.validate_on_submit():
+        r = requests.put(f"{GAMES_SERVER_URL}api/games/{game_id}", json={
+            "description": form.description.data
+        })
+        if r.status_code == 202:
+            flash("Game Editing Succeed", 'success')
+        else:
+            flash("Game Editing Failed", 'danger')
+
+        return redirect(url_for("all_games"))
+    else:
+        game = requests.get(f"{GAMES_SERVER_URL}api/games/{game_id}").json()
+        form.description.data = game.get("description", "")
+        return render_template("gameplay/game_edit.html", form=form, game=game)
+
+
+@app.route("/games/create", methods=("GET", "POST"))
+def create_game():
+    form = AddGameForm()
+
+    if form.validate_on_submit():
+        r = requests.post(f"{GAMES_SERVER_URL}api/games", json={
+            "description": form.description.data,
+        })
+        if r.status_code == 201:
+            flash("Game Adding Succeed", 'success')
+        else:
+            flash("Game Adding Failed", 'danger')
+
+        return redirect(url_for("all_games"))
+    else:
+        return render_template("gameplay/game_create.html", form=form)
 
 
 @app.route("/players", methods=("GET",))
@@ -76,9 +122,43 @@ def particular_player(id: int):
     return render_template("gameplay/player.html", player=player)
 
 
-@app.route("/players/<int:player_id>/edit")
+@app.route("/players/create", methods=("GET", "POST"))
+def create_player():
+    form = AddPlayerForm()
+
+    if form.validate_on_submit():
+        r = requests.post(f"{PLAYERS_SERVER_URL}api/players", json={
+            "username": form.username.data,
+            "image_file": form.image_file.data
+        })
+        if r.status_code == 201:
+            flash("Player Adding Succeed", 'success')
+        else:
+            flash("Player Adding Failed", 'danger')
+
+        return redirect(url_for("all_players"))
+    else:
+        return render_template("gameplay/player_create.html", form=form)
+
+
+@app.route("/players/<int:player_id>/edit", methods=["GET", "POST"])
 def edit_player(player_id: int):
-    return "<h1>Edit Player</h1>"
+    form = EditPlayerForm()
+
+    if form.validate_on_submit():
+        r = requests.put(f"{PLAYERS_SERVER_URL}api/players/{player_id}", json={
+            "username": form.username.data,
+            "image_file": form.image_file.data
+        })
+        if r.status_code == 202:
+            flash("Player Editing Succeed", 'success')
+        else:
+            flash("Player Editing Failed", 'danger')
+
+        return redirect(url_for("all_players"))
+    else:
+        player = requests.get(f"{PLAYERS_SERVER_URL}api/players/{player_id}").json()
+        return render_template("gameplay/player_edit.html", form=form, player=player)
 
 
 @app.route("/about", methods=("GET",))
